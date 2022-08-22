2afbc99ea873be441d6385a4edac85e4a1929f37
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-22 19:26:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
 region = "us-east-2"
-access_key = "AKIAV4HFQJQUT2TKXHKO"
-secret_key = "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"
+#access_key = "AKIAV4HFQJQUT2TKXHKO"
+#secret_key = "uVMn1w9//60rOFeEyIOhN7kB2Hfei7IfOGo3gT6I"
 }
 resource "aws_instance" "example" {
 ami = "ami-02f3416038bdb17fb"
 instance_type = "t2.micro"
 #vpc_security_group_ids = [aws_security_group.instance.id]
-key_name = "terraform"
+#key_name = "terraform"
 tags = {
 Name = "terraform-area51"
 }
