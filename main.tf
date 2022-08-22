0142451906aafce3477c9f9bd560adf63a70bc67
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-22 19:30:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "example" {
 ami = "ami-02f3416038bdb17fb"
 instance_type = "t2.micro"
 #vpc_security_group_ids = [aws_security_group.instance.id]
-#key_name = "terraform"
+key_name = "terraform"
 tags = {
 Name = "terraform-area51"
 }
