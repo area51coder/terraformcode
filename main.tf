3c14344e3701892959eae510c14e0e155b6149fa
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-22 19:37:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-2"
 #access_key = "AKIAV4HFQJQUT2TKXHKO"
 #secret_key = "uVMn1w9//60rOFeEyIOhN7kB2Hfei7IfOGo3gT6I"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "web" {
 ami = "ami-02f3416038bdb17fb"
 instance_type = "t2.micro"
 #vpc_security_group_ids = [aws_security_group.instance.id]
